86bd0bfd194793eeb6f4e6b35706ae6fa63f3e07
brycerobinsn <bryce.robinson@gmail.com>
2022-10-10 21:50:29 -0700

    added winning combinations and restart button

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
+// select elements
+const tiles = document.querySelectorAll('.tile')
+const winningCombinations = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]
+
 console.log('hello world')
 // variable declarations
 const colors = {
@@ -5,6 +18,11 @@ const colors = {
 }
 
 // functions
-
+const tileSelect = function (tile) {
+    console.log('cliocked')
+}
 
 // event listeners
+tiles.forEach(function(tile) {
+    tile.addEventListener('click',tileSelect)
+})
